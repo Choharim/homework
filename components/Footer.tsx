56ecb2a9ba9118abadcf0633118e1c239f3c7276
Choharim <73021666+Choharim@users.noreply.github.com>
2022-09-25 22:21:16 +0900

    refactor: 임시 nav, footer, layout 컴포넌트 수정

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-function Footer() {
-  return <div>Footer</div>
+const Footer = () => {
+  return <footer>© {new Date().getFullYear()}</footer>
 }
 
 export default Footer
