68416318527628d0a0f510b5aeff98ed35e4a1b8
Choharim <73021666+Choharim@users.noreply.github.com>
2022-10-03 11:27:06 +0900

    refactor: navigation 스타일 추가

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,46 @@
 import React from 'react'
 import styled from 'styled-components'
 import Link from 'next/link'
-import { KO_NAME } from 'entity/owner/constant'
+import Image from 'next/image'
 
-export const NAVBAR_HEIGHT = 56
+import { EN_NAME, GITHUB_URL } from 'entity/owner/constant'
+import Logo from 'public/logo.png'
+import GithubLogo from 'public/github_logo.png'
+import { deviceSize } from 'constants/common'
+
+export const NAVBAR_HEIGHT = 65
 
 const Navbar = () => {
   return (
     <Navigation>
-      <Link href="/" passHref>
-        <a>
-          <Name>{KO_NAME.first}</Name>
-        </a>
-      </Link>
+      <Frame>
+        <Link href="/">
+          <a>
+            <LogWrapper>
+              <Image
+                layout="fixed"
+                height={40}
+                width={40}
+                src={Logo}
+                alt="logo_image"
+              />
+              <Name>{`${EN_NAME.first}'s log`}</Name>
+            </LogWrapper>
+          </a>
+        </Link>
+
+        <Link href={GITHUB_URL}>
+          <a target="_blank" rel="noopener noreferrer">
+            <Image
+              layout="fixed"
+              height={30}
+              width={30}
+              src={GithubLogo}
+              alt="github_logo_image"
+            />
+          </a>
+        </Link>
+      </Frame>
     </Navigation>
   )
 }
@@ -20,15 +48,29 @@ const Navbar = () => {
 export default Navbar
 
 const Navigation = styled.nav`
-  display: flex;
-  align-items: center;
   width: 100%;
   height: ${NAVBAR_HEIGHT}px;
-  padding: 0 24px;
 
   background-color: ${({ theme }) => theme.color.lightBlack};
 `
 
+const Frame = styled.div`
+  display: flex;
+  align-items: flex-end;
+  justify-content: space-between;
+  width: 100%;
+  max-width: ${deviceSize.pc}px;
+  margin: 0 auto;
+  padding: 12px 24px;
+`
+
+const LogWrapper = styled.div`
+  display: flex;
+  align-items: end;
+`
+
 const Name = styled.span`
+  margin-left: 10px;
   color: ${({ theme }) => theme.color.white};
+  ${({ theme }) => theme.font.header_3};
 `
