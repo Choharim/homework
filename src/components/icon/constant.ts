8a3540b8355fbfeb2fece8ff494a8bf4a17278bd
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-14 04:40:33 +0900

    feat: icon component, icon svg 추가

---
+++ src/components/icon/constant.ts
@@ -0,0 +1,7 @@
+import ArrowRight from 'public/icons/arrow-right.svg'
+import DoubleArrowRight from 'public/icons/double-arrow-right.svg'
+
+export const ICON_COMPONENT = {
+  ArrowRight,
+  DoubleArrowRight,
+} as const
