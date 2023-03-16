61c10041a9c8095b4daf9cd181da2cd9cf2f15fa
Choharim <73021666+Choharim@users.noreply.github.com>
2023-03-16 18:43:07 +0900

    refactor: pagination 스타일 수정

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,34 +1,41 @@
-import { SVGAttributes } from 'react'
-import { useTheme } from 'styled-components'
+import React, { SVGAttributes } from 'react'
+import styled, { useTheme } from 'styled-components'
 
 import { color } from '@/styles/theme'
 import { ICON_COMPONENT } from './constant'
 
 interface IconProps
-  extends Pick<SVGAttributes<SVGAElement>, 'width' | 'height'> {
+  extends Pick<SVGAttributes<SVGAElement>, 'width' | 'height' | 'className'> {
   type: keyof typeof ICON_COMPONENT
   stroke?: keyof typeof color
   fill?: keyof typeof color
 }
-const Icon = ({
-  type,
-  width = 22,
-  height = 22,
-  stroke = 'black',
-  fill,
-}: IconProps) => {
-  const theme = useTheme()
-  const IconComponent = ICON_COMPONENT[type]
+const Icon = React.forwardRef<HTMLDivElement, IconProps>(
+  (
+    { type, width = 22, height = 22, stroke = 'black', fill, className },
+    ref
+  ) => {
+    const theme = useTheme()
+    const IconComponent = ICON_COMPONENT[type]
 
-  return (
-    <IconComponent
-      width={width}
-      height={height}
-      stroke={theme.color[stroke]}
-      fill={!fill ? 'none' : theme.color[fill]}
-      strokeWidth="2.0"
-    />
-  )
-}
+    return (
+      <Wrapper className={className} ref={ref}>
+        <IconComponent
+          width={width}
+          height={height}
+          stroke={theme.color[stroke]}
+          fill={!fill ? 'none' : theme.color[fill]}
+          strokeWidth="2.0"
+        />
+      </Wrapper>
+    )
+  }
+)
 
 export default Icon
+
+Icon.displayName = 'Icon'
+
+const Wrapper = styled.div`
+  padding: 5px;
+`
