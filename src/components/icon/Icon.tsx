63fe0e370f6e222ae379ce7e90fdb2610bca7eb3
Choharim <73021666+Choharim@users.noreply.github.com>
2023-04-19 11:22:53 +0900

    refactor: copy svg icon 컴포넌트에 추가

--- src/components/icon/Icon.tsx
+++ src/components/icon/Icon.tsx
@@ -1,26 +1,36 @@
-import React, { SVGAttributes } from 'react'
+import React, { HTMLAttributes, SVGAttributes } from 'react'
 import styled, { useTheme } from 'styled-components'
 
 import { COLOR } from '@/styles/theme'
 import { ICON_COMPONENT } from './constant'
 
 interface IconProps
-  extends Pick<SVGAttributes<SVGAElement>, 'width' | 'height' | 'className'> {
+  extends Pick<SVGAttributes<SVGAElement>, 'width' | 'height'>,
+    Pick<HTMLAttributes<HTMLDivElement>, 'className' | 'onClick'> {
   type: keyof typeof ICON_COMPONENT
   stroke?: keyof typeof COLOR
   fill?: keyof typeof COLOR
 }
 const Icon = React.forwardRef<HTMLDivElement, IconProps>(
   (
-    { type, width = 22, height = 22, stroke = 'black', fill, className },
+    {
+      type,
+      width = 22,
+      height = 22,
+      stroke = 'black',
+      fill,
+      className,
+      ...restProps
+    },
     ref
   ) => {
     const theme = useTheme()
     const IconComponent = ICON_COMPONENT[type]
 
     return (
-      <Wrapper className={className} ref={ref}>
+      <Wrapper {...restProps} ref={ref}>
         <IconComponent
+          className={className}
           width={width}
           height={height}
           stroke={theme.color[stroke]}
