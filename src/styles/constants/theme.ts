a2e6212b4685ad6025c352ea3369d837e102efe6
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-25 13:35:32 +0900

    refactor: theme provider에서 color외 제거

--- src/styles/constants/theme.ts
+++ src/styles/constants/theme.ts
@@ -1,11 +1,7 @@
 import { DefaultTheme } from 'styled-components'
 
 import COLOR from './color'
-import FONT from './font'
-import MEDIA from './media'
 
 export const THEME: DefaultTheme = {
   color: COLOR,
-  font: FONT,
-  media: MEDIA,
 }
