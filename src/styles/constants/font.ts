f0067c326e84c9bc7327d0d171eb095b2f5663b8
Choharim <73021666+Choharim@users.noreply.github.com>
2023-05-26 00:34:48 +0900

    refactor: font style 변경

--- src/styles/constants/font.ts
+++ src/styles/constants/font.ts
@@ -1,58 +1,68 @@
 const FONT = {
   header_1: {
-    fontSize: '36px',
+    fontSize: '2.25rem',
     lineHeight: '1.3',
-    fontWeight: 700,
+    fontWeight: 600,
   },
   header_2: {
-    fontSize: '30px',
+    fontSize: '2rem',
     lineHeight: '1.3',
     fontWeight: 600,
   },
   header_3: {
-    fontSize: '26px',
+    fontSize: '1.75rem',
     lineHeight: '1.3',
     fontWeight: 600,
   },
   header_4: {
-    fontSize: '22px',
+    fontSize: '1.5rem',
     lineHeight: '1.3',
     fontWeight: 600,
   },
-  subtitle_1: {
-    fontSize: '20px',
-    lineHeight: '1.7',
-    fontWeight: 600,
+
+  title_1: {
+    fontSize: '1.25rem',
+    lineHeight: '1.6',
+    fontWeight: 500,
   },
-  subtitle_2: {
-    fontSize: '18px',
-    lineHeight: '1.7',
-    fontWeight: 600,
+  title_2: {
+    fontSize: '1.125rem',
+    lineHeight: '1.6',
+    fontWeight: 500,
   },
-  subtitle_3: {
-    fontSize: '16px',
+  title_3: {
+    fontSize: '1rem',
+    lineHeight: '1.7',
+    fontWeight: 500,
+  },
+
+  caption_1: {
+    fontSize: '0.875rem',
+    lineHeight: '1.7',
+    fontWeight: 500,
+  },
+  caption_2: {
+    fontSize: '0.75rem',
+    lineHeight: '1.7',
+    fontWeight: 500,
+  },
+  caption_3: {
+    fontSize: '0.625rem',
     lineHeight: '1.7',
     fontWeight: 500,
   },
 
   body_1: {
-    fontSize: '17px',
+    fontSize: '1rem',
     lineHeight: '1.7',
   },
   body_2: {
-    fontSize: '16px',
-    lineHeight: '1.6',
-    fontWeight: 500,
+    fontSize: '0.875rem',
+    lineHeight: '1.7',
   },
   body_3: {
-    fontSize: '15px',
-    lineHeight: '1.6',
-    fontWeight: 500,
-  },
-  body_4: {
-    fontSize: '14px',
-    lineHeight: '1.6',
-    fontWeight: 500,
+    fontSize: '0.75rem',
+    lineHeight: '1.7',
   },
 } as const
 
