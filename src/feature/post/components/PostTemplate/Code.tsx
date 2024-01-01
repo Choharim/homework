c6fba890439243fb650138560dca8dd69b0cee68
Choharim <jhl8586@naver.com>
2024-01-01 23:05:39 +0900

    refactor: notionCode, renderer 사용

---
+++ src/feature/post/components/PostTemplate/Code.tsx
@@ -0,0 +1,38 @@
+import ToastPortal from '@/components/toast/ToastPortal'
+
+import useToast from '@/components/toast/useToast'
+import { Code as NotionCode } from 'react-notion-x/build/third-party/code'
+import { ComponentProps, useEffect } from 'react'
+import { NotionComponents } from 'react-notion-x'
+
+const COPY_MESSAGE = '클립보드에 복사되었습니다.'
+
+type Props = ComponentProps<NotionComponents['Code']>
+
+const Code = (props: Props) => {
+  const { toasts, addToast } = useToast()
+
+  useEffect(() => {
+    const copyButton = document.querySelector('.notion-code-copy-button')
+
+    if (!copyButton) return
+
+    const handleClickCopy = () => {
+      addToast({ variety: 'normal', desc: COPY_MESSAGE })
+    }
+
+    copyButton.addEventListener('click', handleClickCopy)
+
+    return () => {
+      copyButton.removeEventListener('click', handleClickCopy)
+    }
+  }, [addToast])
+
+  return (
+    <>
+      <ToastPortal toasts={toasts} />
+      <NotionCode {...props} />
+    </>
+  )
+}
+export default Code
