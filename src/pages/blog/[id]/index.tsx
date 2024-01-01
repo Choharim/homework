94657aa83321295639e484ec3c8a0d5d4aa1a61f
Choharim <jhl8586@naver.com>
2024-01-01 23:06:37 +0900

    feat: notion api 사용한 blog 페이지

---
+++ src/pages/blog/[id]/index.tsx
@@ -0,0 +1,104 @@
+import notionAPI from '@/adapter/notion'
+import Layout from '@/components/layout/Layout'
+import MetaHead from '@/components/layout/MetaHead'
+import PostTemplate from '@/feature/post/components/PostTemplate'
+import CustomStyleProvider from '@/feature/post/components/PostTemplate/CustomStyleProvider'
+
+import Outlink from '@/feature/post/components/PostTemplate/Outlink'
+import { NextPageWithLayout } from '@/shared/types/layout'
+import {
+  GetStaticPaths,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+  PreviewData,
+} from 'next'
+import dynamic from 'next/dynamic'
+import Image from 'next/image'
+import Link from 'next/link'
+import { ParsedUrlQuery } from 'querystring'
+import React from 'react'
+import { NotionRenderer } from 'react-notion-x'
+
+const Code = dynamic(
+  () => import('@/feature/post/components/PostTemplate/Code'),
+  { ssr: false }
+)
+
+const PostDetail: NextPageWithLayout<
+  InferGetStaticPropsType<typeof getStaticProps>
+> = ({ post, frontMatter }) => {
+  if (!frontMatter)
+    return (
+      <div>
+        <h3>No data found.</h3>
+      </div>
+    )
+
+  return (
+    <PostTemplate frontMatter={frontMatter}>
+      <CustomStyleProvider>
+        <NotionRenderer
+          recordMap={post}
+          showTableOfContents={false}
+          components={{
+            Code: Code,
+            nextImage: Image,
+            Link: Outlink,
+            nextLink: Link,
+            Collection: () => <></>,
+          }}
+        />
+      </CustomStyleProvider>
+    </PostTemplate>
+  )
+}
+
+export default PostDetail
+
+PostDetail.getLayout = function getLayout(
+  page: React.ReactElement<InferGetStaticPropsType<typeof getStaticProps>>
+) {
+  return (
+    <Layout resetFrameStyle>
+      <MetaHead
+        title={page.props.frontMatter?.title}
+        description={page.props.frontMatter?.description}
+        ogType="article"
+      />
+      {page}
+    </Layout>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const ids = await notionAPI.getPostIDs()
+
+  const paths = ids.map((id) => {
+    return {
+      params: {
+        id,
+      },
+    }
+  })
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export async function getStaticProps(
+  context: GetStaticPropsContext<ParsedUrlQuery, PreviewData>
+) {
+  const id = context.params?.id as string
+  const frontMatter = await notionAPI.getPostFrontMatter(id)
+  const post = await notionAPI.getPost(id)
+
+  return {
+    props: {
+      frontMatter,
+      post,
+    },
+    revalidate: 60,
+  }
+}
---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,51 @@
+import { InferGetStaticPropsType } from 'next'
+import { useRouter } from 'next/router'
+
+import notionAPI from '@/adapter/notion'
+import { NextPageWithLayout } from '@/shared/types/layout'
+
+import CardListFrame from '@/feature/post/components/CardListFrame'
+import PostCardLink from '@/feature/post/components/PostCardLink'
+import CategoryChip from '@/feature/post/components/CategoryChip'
+import postFeature from '@/feature/post'
+
+const Blog: NextPageWithLayout<
+  InferGetStaticPropsType<typeof getStaticProps>
+> = ({ posts }) => {
+  const router = useRouter()
+
+  return (
+    <CardListFrame>
+      {posts?.map((post) => {
+        const { id, title, description, create_date, category } = post
+        return (
+          <PostCardLink key={id} href={`${router.pathname}/${id}`}>
+            <PostCardLink.Top>
+              <PostCardLink.Title>{title}</PostCardLink.Title>
+              <PostCardLink.Desc>{description}</PostCardLink.Desc>
+            </PostCardLink.Top>
+            <PostCardLink.Bottom>
+              <CategoryChip category={category} />
+              <PostCardLink.Date dateTime={create_date} />
+            </PostCardLink.Bottom>
+          </PostCardLink>
+        )
+      })}
+    </CardListFrame>
+  )
+}
+
+export default Blog
+
+export async function getStaticProps() {
+  const allPosts = await notionAPI.getPosts()
+  const publishedPosts = postFeature.getPublishedPosts(allPosts)
+  const posts = postFeature.getPostsSortedByNewest(publishedPosts)
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 60,
+  }
+}
