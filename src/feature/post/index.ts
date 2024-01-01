41e92927b532f55bdda90de4df3449a1d06f9835
Choharim <jhl8586@naver.com>
2024-01-01 23:02:55 +0900

    feat: postFeature 추가

---
+++ src/feature/post/index.ts
@@ -0,0 +1,49 @@
+import { PostCategory, PostFrontMatter, PostTag } from '@/adapter/notion/type'
+
+class PostFeature {
+  private POST_CATEGORY_NAME = {
+    CS: '컴퓨터 공학',
+    FE: '프론트엔드',
+  }
+  public getCategoryName(category: PostCategory) {
+    return this.POST_CATEGORY_NAME[category] ?? ''
+  }
+
+  private POST_TAG_NAME = {
+    network: '네트워크',
+    'data-structure': '자료구조',
+    algorithms: '알고리즘',
+    'operating-system': '운영체제',
+    typescript: '타입스크립트',
+    javascript: '자바스크립트',
+    theory: '이론',
+    'problem-solving': '문제해결',
+    'trouble-shooting': '이슈해결',
+    thinking: '생각',
+  }
+  public getTagName(tag: PostTag) {
+    return this.POST_TAG_NAME[tag] ?? ''
+  }
+
+  public getIsPublished(post: PostFrontMatter) {
+    return !!post.published
+  }
+
+  public getPublishedPosts(posts: PostFrontMatter[]) {
+    return posts.filter((post) => this.getIsPublished(post))
+  }
+
+  public getPostsSortedByNewest(posts: PostFrontMatter[]) {
+    const copiedPosts = [...posts]
+
+    copiedPosts.sort((a, b) => {
+      return Date.parse(b.create_date) - Date.parse(a.create_date)
+    })
+
+    return copiedPosts
+  }
+}
+
+const postFeature = new PostFeature()
+
+export default postFeature
