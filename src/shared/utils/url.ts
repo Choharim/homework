9d74bac6671c3124317589fad708fe9efc891cc7
Choharim <jhl8586@naver.com>
2024-03-17 17:56:59 +0900

    feat: url util 추가

--- src/shared/utils/url.ts
+++ src/shared/utils/url.ts
@@ -1,3 +1,36 @@
+import { getWithoutEmptyField } from './object'
+
 export const getURLRemovedQuery = (url: string) => {
   return url.split('?')[0]
+}
+
+/**
+ * @description
+ * URLSearchParams을 객체 리터럴로 반환합니다.
+ * @example
+ * const params = new URLSearchParams(...)
+ * getSearchParams(params) // { foo: "bar", baz: "bar" }
+ */
+export const getSearchParams = (searchParams: URLSearchParams) => {
+  const params = Object.fromEntries(searchParams.entries())
+
+  return params
+}
+
+/**
+ * @description
+ * key와 value를 = 로, key value pair를 & 로 연결합니다.
+ * @example
+ * const paramsObj = { foo: "bar", baz: "bar" };
+ * getSearchParamString(paramsObj) // "foo=bar&baz=bar"
+ */
+export const getSearchParamString = <
+  T extends Record<string, string> | URLSearchParams
+>(
+  pageParams: T
+) => {
+  const filledObj = getWithoutEmptyField(pageParams)
+  const searchParams = new URLSearchParams(filledObj as T)
+
+  return searchParams.toString()
 }
