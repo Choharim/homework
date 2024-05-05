2c0afa4604ba6c53ab4b0f2ab9cff0a93e9ad7a2
Choharim <jhl8586@naver.com>
2024-05-05 15:28:06 +0900

    refactor: nav fixed -> sticky 변경에 따른 수정

--- src/components/layout/style/footer.css.ts
+++ src/components/layout/style/footer.css.ts
@@ -2,11 +2,9 @@ import COLOR from '@/styles/constants/color'
 import { style } from '@vanilla-extract/css'
 import FONT from '@/styles/constants/font'
 
-export const FOOTER_HEIGHT = 100
-
 export const wrapper = style({
   margin: '0 auto',
   color: COLOR.grey900,
-  height: FOOTER_HEIGHT,
+  height: 100,
   ...FONT.body_1,
 })
