a937c8ca7876ba45851fb5b62f0a2b42eda81bba
Choharim <jhl8586@naver.com>
2024-11-23 19:08:11 +0900

    refactor: 테스트용 코드 제거

--- src/app/(list)/page.tsx
+++ src/app/(list)/page.tsx
@@ -4,7 +4,6 @@ import CardListFrame from '@/feature/post/components/CardListFrame'
 import React, { Suspense } from 'react'
 import * as style from 'src/feature/post/components/style/cardListFrame.css'
 import PostList from './_components/PostList'
-import Typo from '@/components/Typo'
 
 async function Page() {
   const all = await notionAPI.getPostFrontMatters()
@@ -17,74 +16,6 @@ async function Page() {
         <PostList frontMatters={frontMatters} />
       </Suspense>
     </CardListFrame>
-  )
-
-  return (
-    <>
-      <Typo variety="header_1">header_1안녕하세요 반갑습니다.</Typo>
-      <Typo variety="header_2">header_2안녕하세요 반갑습니다.</Typo>
-      <Typo variety="header_3">header_3안녕하세요 반갑습니다.</Typo>
-      <Typo variety="title_1">title_1안녕하세요 반갑습니다.</Typo>
-      <Typo variety="title_2">title_2안녕하세요 반갑습니다.</Typo>
-      <Typo variety="title_3">title_3안녕하세요 반갑습니다.</Typo>
-      <Typo variety="subtitle_1">subtitle_1안녕하세요 반갑습니다.</Typo>
-      <Typo variety="subtitle_2">subtitle_2안녕하세요 반갑습니다.</Typo>
-      <Typo variety="subtitle_3">subtitle_3안녕하세요 반갑습니다.</Typo>
-      <Typo variety="caption_1">caption_1안녕하세요 반갑습니다.</Typo>
-      <Typo variety="caption_2">caption_2안녕하세요 반갑습니다.</Typo>
-
-      <Typo variety="body_1">body_1안녕하세요 반갑습니다.</Typo>
-      <Typo variety="body_2">body_2안녕하세요 반갑습니다.</Typo>
-      <Typo variety="detail_1">detail_1안녕하세요 반갑습니다.</Typo>
-      <Typo variety="detail_2">detail_2안녕하세요 반갑습니다.</Typo>
-
-      <Typo variety="header_1" responsive={false}>
-        header_1안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="header_2" responsive={false}>
-        header_2안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="header_3" responsive={false}>
-        header_3안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="title_1" responsive={false}>
-        title_1안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="title_2" responsive={false}>
-        title_2안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="title_3" responsive={false}>
-        title_3안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="subtitle_1" responsive={false}>
-        subtitle_1안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="subtitle_2" responsive={false}>
-        subtitle_2안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="subtitle_3" responsive={false}>
-        subtitle_3안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="caption_1" responsive={false}>
-        caption_1안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="caption_2" responsive={false}>
-        caption_2안녕하세요 반갑습니다.
-      </Typo>
-
-      <Typo variety="body_1" responsive={false}>
-        body_1안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="body_2" responsive={false}>
-        body_2안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="detail_1" responsive={false}>
-        detail_1안녕하세요 반갑습니다.
-      </Typo>
-      <Typo variety="detail_2" responsive={false}>
-        detail_2안녕하세요 반갑습니다.
-      </Typo>
-    </>
   )
 }
 
