646c2eb02ff7e2526068e480782d64fd617528b7
Choharim <jhl8586@naver.com>
2024-11-24 20:30:34 +0900

    refactor: next heade 제거

--- src/feature/seo/components/StructuredData.tsx
+++ src/feature/seo/components/StructuredData.tsx
@@ -1,22 +1,21 @@
 import React from 'react'
 
-import Head from 'next/head'
-
 import { Thing, WithContext } from 'schema-dts'
 
+/**
+ * @link {https://nextjs.org/docs/app/building-your-application/optimizing/metadata#json-ld}
+ */
 interface Props<T extends Thing> {
   data: WithContext<T>
 }
 function StructuredData<T extends Thing>({ data }: Props<T>) {
   return (
-    <Head>
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{
-          __html: JSON.stringify(data),
-        }}
-      />
-    </Head>
+    <script
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify(data),
+      }}
+    />
   )
 }
 
