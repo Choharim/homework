ef0c04d2306892621943f7f3413b2da96f7f00ca
Choharim <jhl8586@naver.com>
2025-02-01 15:59:31 +0900

    refactor: nav css 수정

--- src/components/Layout/navbar.css.ts
+++ src/components/Layout/navbar.css.ts
@@ -1,16 +1,9 @@
 import { convertHEXToRGB } from '@/shared/_utils'
 import COLOR from '@/styles/color'
 import Z_INDEX from '@/styles/zIndex'
-import { createVar, style } from '@vanilla-extract/css'
+import { style } from '@vanilla-extract/css'
 
 export const NAVBAR_HEIGHT = 60
-
-export const navigationYVar = createVar()
-
-export const NAV_Y = {
-  show: '0',
-  hidden: '-100%',
-}
 
 export const navigation = style({
   position: 'sticky',
@@ -20,11 +13,19 @@ export const navigation = style({
   height: NAVBAR_HEIGHT,
   backgroundColor: `rgb(${convertHEXToRGB(COLOR.white)}, 0.5)`,
   backdropFilter: 'saturate(180%) blur(5px)',
-  borderBottom: `1px solid ${COLOR.grey100}`,
+  borderBottom: `1px solid ${COLOR.grey200}`,
   zIndex: Z_INDEX.nav,
   display: 'block',
   transition: 'transform 0.2s',
-  transform: `translateY(${navigationYVar})`,
+
+  selectors: {
+    '&[data-hidden="true"]': {
+      transform: `translateY(-100%)`,
+    },
+    '&[data-hidden="false"]': {
+      transform: `translateY(0)`,
+    },
+  },
 })
 
 export const navigationFrame = style({
